502e950bcd8e86fc4aa973e100de5ae5284cb91c
Andrew Le <s3820098@rmit.edu.vn>
2022-06-19 09:25:34 +0700

    Update store.js: combine reducers

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
-import messageReducer from './messageSlice';
+// REDUX-PERSIST
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {applyMiddleware, combineReducers} from 'redux';
+import {createLogger} from 'redux-logger';
+import {persistReducer} from 'redux-persist';
 
-export const store = configureStore({
-  reducer: {
-    message: messageReducer
-  }
+import messageReducer from './messageSlice';
+import userReducer from './userSlice';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  message: messageReducer,
 });
+
+// persist config obj
+// blacklist a store attribute using it's reducer name.
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  // Whitelist (Save Specific Reducers)
+  whitelist: ['userReducer'],
+  // Blacklist (Don't Save Specific Reducers)
+  blacklist: ['messageReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(createLogger()));
+
+// Middleware: Redux Persist Persister
+let persistor = persistStore(store);
+
+// Exports
+export {store, persistor};
